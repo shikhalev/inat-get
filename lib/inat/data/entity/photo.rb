e62237e3da0a72b3cd310a610115c8c33fbb08ae
Иван Шихалев <shikhalev@gmail.com>
2023-10-25 16:11:26 +0500

    Ср 25 окт 2023 16:11:26 +05

--- lib/inat/data/entity/photo.rb
+++ lib/inat/data/entity/photo.rb
@@ -10,6 +10,22 @@ class Photo < Entity
 
   table :photos
 
-  # TODO: implement
+  field :license_code, type: Symbol, index: true
+  field :url, type: URI, required: true
+  field :square_url, type: URI
+  field :medium_url, type: URI
+  field :small_url, type: URI
+  field :large_url, type: URI
+  field :original_url, type: URI
+  field :attribution, type: String
+  field :hidden, type: Boolean, index: true
+
+  links :flags, item_type: Flag
+
+  ignore :original_dimensions                     # TODO: сделать нормальный тип
+
+  ignore :moderator_actions                       # TODO: разобраться
+  field :native_page_url, type: URI
+  ignore :native_photo_id
 
 end
--- lib/inat/data/entity/place.rb
+++ lib/inat/data/entity/place.rb
@@ -2,6 +2,7 @@
 
 require_relative '../types/std'
 require_relative '../types/extras'
+require_relative '../types/location'
 require_relative '../entity'
 
 autoload :Observation, 'inat/data/entity/observation'
@@ -11,6 +12,22 @@ class Place < Entity
   path :places
   table :places
 
-  # TODO: implement
+  field :uuid, type: UUID, unique: true
+  field :name, type: String, index: true, required: true
+  field :slug, type: Symbol, index: true
+  field :display_name, type: String, index: true
+  field :bbox_area, type: Float
+  field :admin_level, type: Integer, index: true
+  field :place_type, type: Integer, index: true
+  field :location, type: Location
+
+  ignore :geometry_geojson                  # TODO: implement
+  ignore :bounding_box_geojson
+
+  links :ancestor_places, item_type: Place, table_name: :place_ancestors, link_field: :ancestor_id, index: true
+
+  def === other
+    self.id == other.id && other.ancestor_place_ids.include?(self.id)
+  end
 
 end
